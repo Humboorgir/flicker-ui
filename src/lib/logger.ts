dae1e9d76cc74d6156086f108d5cd06d1957b6c0
Humboorgir <hardcraftap@gmail.com>
2025-02-22 20:31:22 +0330

    refactor: modify logger style

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -13,22 +13,25 @@ const defaultOptions = {
 export const loggerMessage = {
   error: (message: string, { raw }: LoggerOptions = defaultOptions) => {
     if (raw) return chalk.red(message);
-    return chalk.red.underline.bold("ERROR") + " " + chalk.red(message);
+    return chalk.red.bold("ERROR") + " " + chalk.red(message);
   },
 
   success: (message: string, { raw }: LoggerOptions = defaultOptions) => {
     if (raw) return chalk.green(message);
-    return chalk.green.underline.bold("SUCCESS") + " " + chalk.green(message);
+    return chalk.green.bold("SUCCESS") + " " + message;
   },
 
   alert: (message: string, { raw }: LoggerOptions = defaultOptions) => {
     if (raw) return chalk.yellow(message);
-    return chalk.yellow.underline.bold("ALERT") + " " + chalk.yellow(message);
+    return chalk.yellow.bold("ALERT") + " " + message;
   },
 };
 
 // logs the formatted string to the console
 const logger = {
+  raw: (message: string) => {
+    console.log(message);
+  },
   error: (message: string, { ...options }: LoggerOptions = defaultOptions) => {
     console.log(loggerMessage.error(message, { ...options }));
   },
